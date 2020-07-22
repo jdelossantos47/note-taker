2dc1f3eb3e2e70ea378c1cca24a4a706c68680d1
jonathan <toleto2013@gmail.com>
2020-07-22 19:33:36 -0400

    added a dependencies path

---
+++ server.js
@@ -0,0 +1,20 @@
+// Require Dependencies
+const express = require("express");
+
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//Require routes file
+require('./routes/routes')(app);
+
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
